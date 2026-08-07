63e1500b0e1ceefb88cb87c7ef7844d66d5bc92f
Anurag Dayal <121846459+AnuragDayal94@users.noreply.github.com>
2026-08-07 22:42:31 +0530

    Time: 425 ms (5.07%), Space: 150.8 MB (5.2%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,86 +1,56 @@
 class Solution {
 public:
 
-    void bfs(vector<vector<int>>& mat,queue<pair<int,int>>& q,vector<vector<bool>>& vis){
-        int m=mat.size();
-        int n=mat[0].size();   
-     
-        int dir[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
-        while(!q.empty()){
-            pair<int,int>p=q.front();
-            q.pop();
-           
-                //trevel
-                int x=p.first;
-                int y=p.second;
-
-                for(auto &d:dir){
-                    int nx=x+d[0];
-                    int ny=y+d[1];
-
-                    if(nx>=0 && nx<m && ny>=0 && ny<n && !vis[nx][ny] && mat[nx][ny]==1){
-                    q.push({nx,ny});
-                    vis[nx][ny]=true;
-                    mat[nx][ny]=mat[x][y]+1;
-                }
-
-                }
-                
-                // //left
-                // if(y-1>=0 && !vis[x][y-1] && mat[x][y-1]==1){
-                //     q.push({x,y-1});
-                //     vis[x][y-1]=true;
-                //     mat[x][y-1]=mat[x][y]+1;
-                // }
-               
-
-                // //up
-                // if(x-1>=0 && !vis[x-1][y] && mat[x-1][y]==1){
-                //     q.push({x-1,y});
-                //     vis[x-1][y]=true;
-                //     mat[x-1][y]=mat[x][y]+1;
-                // }
-            
-
-                // //right
-                // if(y+1<n && !vis[x][y+1] && mat[x][y+1]==1){
-                //     q.push({x,y+1});
-                //     vis[x][y+1]=true;
-                //     mat[x][y+1]=mat[x][y]+1;
-                // }
-               
-
-                // //down
-                // if(x+1<m && !vis[x+1][y] && mat[x+1][y]==1){
-                //     q.push({x+1,y});
-                //     vis[x+1][y]=true;
-                //     mat[x+1][y]=mat[x][y]+1;
-                // }
-
-            
-        }
-       
-    }
-
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        // vector<vector<int>> ans(mat.size(),vector<int>(mat[0].size(),0));
+    void solve(vector<vector<int>>& mat, queue<pair<int,int>>&q, vector<vector<bool>>&vis){
         int m=mat.size();
         int n=mat[0].size();
         
-        queue<pair<int,int>>q;
-        vector<vector<bool>>vis(m,vector<bool>(n,false));
-        for(int i=0; i<mat.size(); i++){
-            for(int j=0; j<mat[0].size(); j++){
-                if(mat[i][j]==0){
-                    q.push({i,j});
-                    vis[i][j]=true;
-                    }                
-            }
-            
-        }
-        bfs(mat,q,vis);
 
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+
+            int x=it.first;
+            int y=it.second;
+
+            vector<vector<int>>v{{1,0},{0,1},{-1,0},{0,-1}};
+
+            for(int i=0; i<4; i++){
+                int x1=x+v[i][0];
+                int y1=y+v[i][1];
+
+                if(x1>=0 && x1<m && y1>=0 && y1<n && !vis[x1][y1] && mat[x1][y1]==1){
+                        q.push({x1,y1});
+                        vis[x1][y1]=true;
+                        mat[x1][y1]=mat[x][y]+1;                      
+                    
+                }
+            }
+
+        }
+        return ;
+
+
+    }
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        // vector<vector<int>>ans(m, vector<int>(n));
+        queue<pair<int,int>>q;
+        vector<vector<bool>>vis(m, vector<bool>(n, false));
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j]==0){
+                    vis[i][j]=true;
+                    q.push({i,j});
+                    
+                }
+                
+            }
+        }
+        solve(mat,q,vis);
         return mat;
-        
     }
 };
