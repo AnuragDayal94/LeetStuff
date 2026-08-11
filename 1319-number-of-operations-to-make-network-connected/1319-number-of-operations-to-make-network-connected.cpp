fa88cc2a517f16f84eb7027816fb101513a30dd5
Anurag Dayal <121846459+AnuragDayal94@users.noreply.github.com>
2026-08-12 01:54:12 +0530

    Time: 13 ms (60.77%), Space: 44.9 MB (59.91%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,59 @@
+class ds{
+    public:
+    vector<int>parent;
+    vector<long long>rank;
+
+    ds(int n){
+        parent.resize(n);
+        rank.resize(n,0);
+        for(int i=0; i<n; i++){
+            parent[i]=i;
+        }
+    }
+
+    int find_par(int u){
+        if(parent[u]==u)return u;
+        return parent[u]=find_par(parent[u]);
+    }
+
+    void unionr(int u, int v){
+        int par_u=find_par(u);
+        int par_v=find_par(v);\
+
+        if(rank[par_u]<rank[par_v]){
+            parent[par_u]=par_v;
+
+        }else if(rank[par_u]>rank[par_v]){
+            parent[par_v]=par_u;
+
+        }else{
+            parent[par_v]=par_u;
+            rank[par_u]++;
+        }
+        return ;
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int c=connections.size();
+        ds d(n);
+        if(c<n-1)return -1;
+        for(int i=0; i<c; i++){
+            int u=connections[i][0];
+            int v=connections[i][1];
+
+            d.unionr(u,v);
+        }
+
+        set<int>st;
+        for(int i=0; i<n; i++){
+            int curr=d.find_par(d.parent[i]);
+            st.insert(curr);
+
+        }
+
+        return st.size()-1;
+    }
+};
