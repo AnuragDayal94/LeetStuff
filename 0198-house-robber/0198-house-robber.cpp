e1c6fe34ec348b3e398fc8d8e5476ce17c9fef40
Anurag Dayal <121846459+AnuragDayal94@users.noreply.github.com>
2026-08-20 11:27:50 +0530

    Time: 0 ms (100%), Space: 10.7 MB (24.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,7 +3,7 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return nums[0];
-        vector<int>dp(n+2,0);
+        vector<int>dp(n+1,0);
         dp[n-1]=nums[n-1];
 
         for(int i=n-2; i>=0; i--){
