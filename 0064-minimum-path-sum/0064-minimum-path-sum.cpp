2a7d55a368a3c7b9864df9441469495268954277
Anurag Dayal <121846459+AnuragDayal94@users.noreply.github.com>
2026-08-21 03:47:17 +0530

    Time: 0 ms (100%), Space: 15.3 MB (90.73%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -3,37 +3,22 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
+        vector<int>next(n+1,INT_MAX);
+        vector<int>curr(n+1,INT_MAX);
 
-        vector<vector<int>>dis(m,vector<int>(n,INT_MAX));
-        dis[0][0]=grid[0][0];
-
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>q;
-        q.push({dis[0][0],0,0});
-        while(!q.empty()){
-            auto it=q.top();
-            q.pop();
-
-            int val=it[0];
-            int x=it[1];
-            int y=it[2];
-
-            if(val>dis[x][y])continue;
-            vector<vector<int>>v={{0,1},{1,0}};
-            for(auto j:v){
-                int x1=x+j[0];
-                int y1=y+j[1];
-
-                if(x1>=0 && x1<m && y1>=0 && y1<n){
-                    if((val+grid[x1][y1])<dis[x1][y1]){
-                        dis[x1][y1]=(val+grid[x1][y1]);
-                        q.push({dis[x1][y1],x1,y1});
-                    }
-                }
-            }
-
+        int psum=0;
+        for(int i=n-1; i>=0; i--){
+            psum+=grid[m-1][i];
+            next[i]=psum;
         }
 
-        return dis[m-1][n-1];
-        
+        for(int i=m-2; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                curr[j]=grid[i][j]+min(curr[j+1],next[j]);
+            }
+            next=curr;
+        }
+        return next[0];
+
     }
 };
