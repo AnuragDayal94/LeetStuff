a8238382cb8b67fd38a81877cae6093571af9421
Anurag Dayal <121846459+AnuragDayal94@users.noreply.github.com>
2026-08-20 11:30:24 +0530

    Time: 0 ms (100%), Space: 10 MB (90.3%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,13 +3,17 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return nums[0];
-        vector<int>dp(n+1,0);
-        dp[n-1]=nums[n-1];
-
+        // vector<int>dp(n+1,0);
+        // dp[n-1]=nums[n-1];
+        int n1=nums[n-1];
+        int n2=0;
+          
         for(int i=n-2; i>=0; i--){
-            dp[i]=max(dp[i+1],(dp[i+2]+nums[i]));
+            int curr=max(n1,(n2+nums[i]));
+            n2=n1;
+            n1=curr;
         }
 
-        return dp[0];
+        return n1;
     }
 };
