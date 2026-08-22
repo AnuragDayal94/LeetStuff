ef0d66707b3e9d64b9b2a45ae2da856776d8cbc6
Anurag Dayal <121846459+AnuragDayal94@users.noreply.github.com>
2026-08-23 05:12:56 +0530

    Time: 15 ms (92.58%), Space: 18 MB (73.83%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -19,9 +19,33 @@ public:
         return dp[amount]=ans;
     }
 
-    int coinChange(vector<int>& coins, int amount) {
+    int solve2(vector<int>& coins, int amount){
         vector<int>dp(amount+1,-1);
-        int ans=solve(coins, amount, dp);
+        dp[0]=0;
+
+        for(int j=1; j<=amount; j++){
+            int ans=INT_MAX;
+
+            for(int i=0; i<coins.size(); i++){
+                int co=coins[i];
+                if(co<=j){
+                    int temp=dp[j-co];
+                    if(temp!=INT_MAX){
+
+                        ans=min(ans,1+temp);
+                    }
+                }
+            }
+
+            dp[j]=ans;
+        }
+
+        return dp[amount];
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        // vector<int>dp(amount+1,-1);
+        int ans=solve2(coins, amount);
         if(ans==INT_MAX)return -1;
         return ans;
     }
