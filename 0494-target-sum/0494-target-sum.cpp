a59213cc7bccc57b45b3ad1bf1cf621d9b9128ee
Anurag Dayal <121846459+AnuragDayal94@users.noreply.github.com>
2026-08-23 06:39:09 +0530

    Time: 22 ms (41.2%), Space: 26.4 MB (19.79%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -11,11 +11,38 @@ public:
         return dp[i][sum+k]=solve(nums, target, i+1, sum+nums[i], dp, k)+solve(nums, target, i+1, sum-nums[i], dp, k);
     }
 
+    int solve2(vector<int>& nums, int target, int k, int total){
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(total,0));
+        if(target < -k || target > k)
+            return 0;
+        dp[n][target+k]=1;
+
+        
+        for(int i=n-1; i>=0; i--){
+            for(int j=-k; j<=k; j++){
+                int t1=j+nums[i];
+                int t2=j-nums[i];
+                int w=0;
+                if(t1>=-k && t1<=k){
+                    w+=dp[i+1][j+nums[i]+k];
+                }
+                if(t2>=-k && t2<=k){
+                    w+=dp[i+1][j-nums[i]+k];
+                }
+                dp[i][j+k]=w;
+            }
+        }
+
+        return dp[0][k];
+
+    }
+
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int k=accumulate(nums.begin(), nums.end(),0);
         int total=2*k + 1;
-        vector<vector<int>>dp(n+1,vector<int>(total,-1));
-        return solve(nums, target, 0, 0, dp, k);
+        
+        return solve2(nums, target, k, total);
     }
 };
