ade6288f1f3f04cafcca7c7824c518fe39521a87
Anurag Dayal <121846459+AnuragDayal94@users.noreply.github.com>
2026-08-25 01:28:26 +0530

    Time: 13 ms (33.92%), Space: 21.2 MB (9.78%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -15,9 +15,28 @@ public:
         return dp[i][have][k]=max(solve(prices, i+1, have, k, dp), -prices[i]+solve(prices, i+1, !have, k-1, dp));
     }
 
+    int solve2(int k, vector<int>& prices){
+        int n=prices.size();
+        vector<vector<int>>curr(2, vector<int>(2*k +1));
+        vector<vector<int>>next(2, vector<int>(2*k +1,0));
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<2; j++){
+                for(int z=1; z<=2*k; z++){
+                    if(j)curr[j][z]=max(next[j][z],prices[i]+curr[!j][z-1]);
+                    else curr[j][z]=max(next[j][z],-prices[i]+curr[!j][z-1]);
+                }
+            }
+            next=curr;
+        }
+
+        return next[0][2*k];
+
+    }
+
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
         vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2, vector<int>(2*k +1,-1)));
-        return solve(prices, 0, false, 2*k, dp);
+        return solve2(k, prices);
     }
 };
