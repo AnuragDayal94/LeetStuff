1ba26745e514634f4e3cd6244036fb56a751c365
Anurag Dayal <121846459+AnuragDayal94@users.noreply.github.com>
2026-08-26 15:39:19 +0530

    Time: 30 ms (57.71%), Space: 17.6 MB (10%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -5,31 +5,35 @@ public:
         vector<int>dp(n,1);
         vector<int>count(n,1);
 
-        int index=0;
+
         int ans=1;
+        
         for(int i=1; i<n; i++){
             for(int j=0; j<i; j++){
-                if(nums[j]<nums[i]){
-                    if(dp[j]+1>dp[i]){
-                        dp[i]=1+dp[j]; 
-                        count[i]=count[j];                       
-                    }else if(dp[j]+1==dp[i]){
-                        count[i]+=count[j];
-                    }
+                if(nums[i]>nums[j] && dp[j]+1>dp[i]){
+                                       
+                        dp[i]=1+dp[j];
+                        count[i]=count[j];
+
+                                                
+                }else if(nums[i]>nums[j] && dp[j]+1==dp[i]){
+                        count[i]=count[i]+count[j];
                 }
             }
-            if(dp[i]>ans){
+            if(dp[i]>ans ){
                 ans=dp[i];
-                index=i;
-            }
-        }
-        int an=0;
-        for(int i=0; i<n; i++){
-            if(dp[i]==ans){
-                an+=count[i];
+                
             }
         }
 
-        return an;
+        int c=0;
+        for(int i=0; i<n; i++){
+           if(dp[i]==ans){
+            c=c+count[i];
+           }
+        }
+        // cout<<ans<<endl;
+        // if(ans==1)return n;
+        return c;
     }
 };
