0a6f3c1bc411391fa384a4b1b6f15485ed267d77
Anurag Dayal <121846459+AnuragDayal94@users.noreply.github.com>
2026-09-01 16:09:59 +0530

    Time: 35 ms (86.95%), Space: 12.1 MB (32.63%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,47 +1,36 @@
 class Solution {
 public:
 
-    bool isPalindrome(string& s,int i,int j){
-        while(i<=j){
-            if(s[i] != s[j]){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
-    int solve(string & s,int j,int index,vector<vector<int>>& dp){
-        //base case : 
-        if(index >= s.size()){
-            if(j >= s.size()){
-                return 0;
-            }
-            else{
-                return INT_MAX;
-            }
-        }
-
-        if(dp[j][index] != -1){
-            return dp[j][index];
-        }
-        //case 1 : 
-        int ans1 = INT_MAX;
-        if(isPalindrome(s,j,index) == true){
-            // cout<<"HI"<<endl;
-            ans1 = 1 + solve(s,index+1,index+1,dp);
-        }
-
-        //case 2 :
-        int ans2 = solve(s,j,index+1,dp);
-
-        return dp[j][index] = min(ans1,ans2);
-    }
+    
     int minCut(string s) {
-        int index = 0;
-        int j = 0;
-        vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1)); 
-        int ans = solve(s,j,index,dp);
-        return ans-1;
+        int n=s.length();
+        vector<vector<bool>> pal(n, vector<bool>(n, false));
+
+        
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = i; j < n; j++) {
+
+                if(s[i] == s[j] &&
+                   (j - i <= 1 || pal[i + 1][j - 1])) {
+                    pal[i][j] = true;
+                }
+            }
+        }
+        vector<int>dp(n+1);
+
+        for(int i=0; i<n; i++){
+            if(pal[0][i]){
+                dp[i]=0;
+                continue;
+            }
+            dp[i]=i;
+            for(int j=1; j<=i; j++){
+                if(pal[j][i]){
+                    dp[i]=min(dp[i], 1+dp[j-1]);
+                }
+            }
+        }
+
+        return dp[n-1];
     }
 };
